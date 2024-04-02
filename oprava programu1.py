c6167197828d55a0a9df9e8d626936138aed7e09
ondrejvotruba <150782499+ondrejvotruba@users.noreply.github.com>
2024-04-02 09:14:41 +0200

    Create oprava programu1.py

---
+++ oprava programu1.py
@@ -0,0 +1,21 @@
+'''
+Vytvoření tabulky sinusových hodnot po 30 stupních
+sin(000°) = +0.00
+sin(030°) = +0.50
+sin(060°) = +0.87
+sin(090°) = +1.00
+sin(120°) = +0.87
+sin(150°) = +0.50
+sin(180°) = +0.00
+sin(210°) = -0.50
+sin(240°) = -0.87
+sin(270°) = -1.00
+sin(300°) = -0.87
+sin(330°) = -0.50
+sin(360°) = -0.00
+'''
+import math
+
+for angle in range(0, 361, 30):
+    sin_value = round(math.sin(math.radians(angle)),2)
+    print(f"sin({angle:03}°) = {sin_value:+.2f}")
